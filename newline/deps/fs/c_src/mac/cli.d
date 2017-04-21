e94f31bf61725d6eda6f3ed1e627dbf7304a3da0
Ari <ari@fullstack.io>
2017-04-21 10:27:32 -0700

    Removed deps

--- newline/deps/fs/c_src/mac/cli.d
+++
@@ -1,5 +0,0 @@
-/Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/cli.o: \
-  /Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/cli.c \
-  /Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/cli.h \
-  /Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/common.h \
-  /Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/compat.h
--- newline/deps/fs/c_src/mac/compat.d
+++
@@ -1,3 +0,0 @@
-/Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/compat.o: \
-  /Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/compat.c \
-  /Users/auser/Development/elixir/mine/sellex/micro/newline/deps/fs/c_src/mac/compat.h

a66aef56646982e1eea830d4899c3b006dd1180a
Ari <ari@fullstack.io>
2017-06-12 13:42:41 -0700

    WIP

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -9,32 +9,6 @@ import PublicMenu from "./PublicMenu";
 
 import styled from "styled-components";
 
-// export const Header = ({ isAuthenticated, ...rest}) => (
-//   <header>
-//     <div className="navbar">
-//       <div className="container-fluid">
-//         {/* TODO: Mobile */}
-//         <div className="navbar-header">
-//           <div className="navbar-brand">
-//             <Link to="/">
-//               Fullstack edu
-//             </Link>
-//           </div>
-//         </div>
-
-//         <div className="collapse navbar-collapse">
-//             {
-//               isAuthenticated ?
-//                 <AuthMenu {...rest} />
-//                 :
-//                 <PublicMenu {...rest} />
-//             }
-//         </div>
-//       </div>
-//     </div>
-//   </header>
-// )
-
 export const Header = ({ isAuthenticated, className, ...rest }) =>
   <header
     className={classnames("navbar navbar-inverse navbar-static-top affix-top", className)}
